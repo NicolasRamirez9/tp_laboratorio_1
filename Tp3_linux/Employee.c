8e9d63f56f4f774206cfb919dc2dd64fadaad7f2
Nico Ramirez <40794868+NicolasRamirez9@users.noreply.github.com>
2019-06-04 10:31:55 -0300

    Add files via upload

---
+++ Tp3_linux/Employee.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+
+Employee* employee_new(void)
+{
+    return (Employee*) malloc(sizeof(Employee));
+}
+
+Employee* employee_newParametros(char* idStr,
+                                 char* nombreStr,
+                                 char* horasTrabajadasStr,
+                                 char* sueldoStr)
+{
+    Employee* retorno = NULL;
+    Employee* pAuxEmployee;
+
+    if( id != NULL && nombre != NULL &&
+        horasTrabajadasStr != NULL && sueldo != NULL)
+    {
+        pAuxEmployee = Emp_new();
+        if(pAuxEmployee != NULL)
+        {
+            if( !employee_setNombre(pAuxEmployee, nombreStr) &&
+                !employee_setHorasTrabajadas(pAuxEmployee, horasTrabajadasStr) &&
+                !employee_setIdStr(pAuxEmployee, idStr) &&
+                !employee_setSueldoStr(pAuxEmployee, sueldoStr))
+            {
+                retorno = pAuxEmployee;
+            }
+            else
+            {
+                employee_delete(pAuxEmployee);
+            }
+        }
+    }
+}
+
+void employee_delete()
+{
+    Employee* this;
+
+    if(this != NULL)
+    {
+        free(this);
+    }
+}
+
+int employee_setId(Employee* this, int id)
+{
+    int retorno = -1;
+    if(this != NULL && id >= 0)
+    {
+        this->id = id;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_getId(Employee* this, int* id)
+{
+    int retorno = -1;
+    if(this != NULL && resultado != NULL)
+    {
+        *resultado = this->id;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_setNombre(Employee* this, char* nombre)
+{
+    int retorno = -1;
+    if(this != NULL && nombre >= 0)
+    {
+        strcpy(this->nombre,nombre);
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_getNombre(Employee* this, char* nombre)
+{
+    int retorno = -1;
+    if(this != NULL && resultado != NULL)
+    {
+        strcpy(this->nombre,resultado);
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_setHorasTrabajadas(Employee* this, int horasTrabajadas)
+{
+    int retorno = -1;
+    if(this != NULL && horasTrabajadas >= 0)
+    {
+        this->horasTrabajadas = horasTrabajadas;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_getHorasTrabajadas(Employee* this, int* horasTrabajadas)
+{
+    int retorno = -1;
+    if(this != NULL && horasTrabajadas != NULL)
+    {
+        *horasTrabajadas = this->horasTrabajadas;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_setSueldo(Employee* this, int sueldo)
+{
+    int retorno = -1;
+    if(this != NULL && sueldo >= 0)
+    {
+        this->sueldo = sueldo;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_getSueldo(Employee* this, int* sueldo)
+{
+    int retorno = -1;
+    if(this != NULL && sueldo != NULL)
+    {
+        *sueldo = this->sueldo;
+        retorno = 0;
+    }
+    return retorno;
+}
---
+++ Tp3_linux/validaciones.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "validaciones.h"
+
+int getString(char* msg,char* msgE,int minimo,int maximo,int reintentos,char* resultado)
+{
+    int ret = -1;
+    char bufferStr[400];
+    if(msg != NULL && msgE != NULL && resultado != NULL && reintentos >=0 && maximo > minimo)
+    {
+        do
+        {
+            printf("\n%s",msg);
+            fflush(stdin);
+            fgets(bufferStr,sizeof(bufferStr),stdin);
+            bufferStr[strlen(bufferStr)-1] = '\0';
+            if(strlen(bufferStr) >= minimo && strlen(bufferStr)<maximo)
+            {
+                strncpy(resultado,bufferStr,maximo);
+                ret = 0;
+                break;
+            }
+            else
+            {
+                printf("\n%s",msgE);
+            }
+            reintentos --;
+        }
+        while(reintentos>0);
+    }
+    return ret;
+}
+
+int getInt(char *msg, char *msgE, int minimo, int maximo, int reintentos, char *resultado)
+{
+    char bufferStr[20];
+    int ret = -1;
+    while(ret == -1 && reintentos > 0)
+    {
+        if(!getString(msg, msgE, minimo, maximo, reintentos, bufferStr)&&(resultado != NULL)&&(isValidint(bufferStr)))
+        {
+            strncpy(resultado,bufferStr,sizeof(bufferStr));
+            ret = 0;
+        }
+        else
+        {
+            printf("%s",msgE);
+            ret = -1;
+        }
+    }
+    return ret;
+}
+
+int getFloat(char *msg, char *msgE, char minimo, char maximo, int reintentos, char *resultado)
+{
+    char bufferStr[20];
+    int ret = -1;
+    while(ret == -1 && reintentos > 0)
+    {
+        if(!getString(msg, msgE, minimo, maximo, reintentos, bufferStr)&&(isValidFloat(bufferStr)))
+        {
+            strncpy(resultado,bufferStr,sizeof(bufferStr));
+            ret = 0;
+        }
+        else
+        {
+            printf("%s", msgE);
+            ret = -1;
+        }
+    }
+    return ret;
+
+}
+
+
+int getName(char* pStr, char* msg, char* msgE,int minimo,int maximo,int reintentos)
+{
+    int ret = -1;
+    char bufferStr[4000];
+    if(msg != NULL && msgE != NULL && pStr != NULL && reintentos >=0 && maximo > minimo)
+    {
+        if(!getString(msg,msgE,minimo,maximo,reintentos,bufferStr))
+        {
+            if(isValidName(bufferStr))
+            {
+                strncpy(pStr, bufferStr, maximo);
+                ret = 0;
+            }
+        }
+    }
+    return ret;
+}
+
+int isValidint(char* cadena)
+{
+    int ret = 1;
+    int i;
+    for(i=0; cadena[i] != '\0'; i++)
+    {
+        if(cadena[i] > '9' || cadena[i] < '0')
+        {
+            ret = 0;
+            break;
+        }
+    }
+    return ret;
+}
+
+int isValidFloat (char* pStr)
+{
+    int i=0;
+    int contadorPuntos=0;
+    while(pStr[i]!='\0')
+    {
+        if((pStr[i]<'0' || pStr[i]>'9')&&(pStr[i]!='.'))
+        {
+
+            return 0;
+        }
+        if(pStr[i]=='.')
+        {
+            contadorPuntos++;
+            if(contadorPuntos>1)
+            {
+                return 0;
+            }
+        }
+        i++;
+    }
+    return 1;
+}
+
+int isValidName(char* cadena)
+{
+    int ret = 1;
+    int i;
+    for(i=0; cadena[i] != '\0'; i++)
+    {
+        if((cadena[i] > 'Z' || cadena[i] < 'A') && (cadena[i] > 'z' || cadena[i] < 'a'))
+        {
+            ret = 0;
+            break;
+        }
+    }
+    return ret;
+}
